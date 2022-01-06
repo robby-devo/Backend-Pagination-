70d974b992cfe3f6a4f8f796420b30eb19bcc774
Robby <robbydongred2@gmail.com>
2022-01-06 17:35:17 +0530

    Started Pagination

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const productController = require("./controllers/product.controller");
+
+const app = express();
+
+app.use(express.json());
+app.use("/products", productController);
+module.exports = app;
---
+++ src/server.js
@@ -0,0 +1,8 @@
+const app = require("./index");
+
+const connect = require("./configs/db");
+
+app.listen(2345, async () => {
+  await connect();
+  console.log("listening on 2345");
+});
